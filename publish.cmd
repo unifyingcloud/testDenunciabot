21f417ca60901305ae8ffcba994b8034c4df1d78
Jonatan <jonatane.cisneros@gmail.com>
2018-04-01 20:08:34 +0200

    testDenunciabot creation

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=testdenunciabot-Web-Deploy.pubxml -p:Password=5d2EtigaN9vtjEKGe090NThQnDYdN6pqFEXnaTxlgikYRaY8FidEiqBjybzg
+
